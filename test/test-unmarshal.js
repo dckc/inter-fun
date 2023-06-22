356da78498653f3190e9925ca574d0fe5d9c927e
Dan Connolly <dckc@madmode.com>
2023-06-22 13:14:18 -0500

    test: refine unmarshal property test for discussion

--- test/test-unmarshal.js
+++ test/test-unmarshal.js
@@ -1,46 +1,49 @@
 // @ts-check
 import '@endo/init';
 import { makeMarshal } from '@endo/marshal';
-import { arbPassable } from '@endo/pass-style/tools.js';
 import { keyEQ } from '@endo/patterns';
 
 import { testProp, fc } from '@fast-check/ava';
 
+// compare with:
+// import { arbPassable } from '@endo/pass-style/tools.js';
 import { arbKey } from './arbPassableKey.js';
-import { makeIdContext, makeUnmarshal } from '../gapp/unmarshal.js';
+import { makeMarshal as customMakeMarshal } from '../gapp/unmarshal.js';
+import { makeProvideRemotable } from '../gapp/boardClient.js';
 
-const opts = /** @type {const} */ ({ serializeBodyFormat: 'smallcaps' });
+const smallcapsOpts = /** @type {const} */ ({
+  serializeBodyFormat: 'smallcaps',
+});
 
-const makeIDPreservingMarshaller = () => {
+const makeProvideSlot = () => {
   const valToSlot = new Map();
-  const m = makeMarshal(
-    (v) => {
-      if (valToSlot.has(v)) {
-        return valToSlot.get(v);
-      }
-      const ix = valToSlot.size;
-      const boardID = `board0${ix}`;
-      valToSlot.set(v, boardID);
-      return boardID;
-    },
-    undefined,
-    opts
-  );
-  return m;
+  const provideSlot = (v) => {
+    if (valToSlot.has(v)) {
+      return valToSlot.get(v);
+    }
+    const ix = valToSlot.size;
+    const boardID = `board0${ix}`;
+    valToSlot.set(v, boardID);
+    return boardID;
+  };
+  return provideSlot;
 };
 
 testProp(
   'marshal.unserialize re-implementation handles all keys',
   [arbKey],
   (t, k) => {
-    const m = makeIDPreservingMarshaller();
-    const ctx = makeUnmarshal(makeIdContext());
+    // reference implementation
+    const m = makeMarshal(makeProvideSlot(), undefined, smallcapsOpts);
     const { body, slots } = m.toCapData(k);
-    console.log('@@@', { body, slots });
+
+    // code under test
+    const ctx = customMakeMarshal(undefined, makeProvideRemotable());
+
     const k2 = ctx.unserialize({ body, slots });
     const k3 = ctx.unserialize({ body, slots });
-    t.deepEqual(k, k2, 'original deepEqual 1st unserialize');
-    t.deepEqual(k, k3, 'original deepEqual 2nd unserialize');
-    t.true(keyEQ(k2, k3), '1st, 2nd keyEQ');
+    t.deepEqual(k, k2, 'arbKey deepEqual 1st custom unmarshal');
+    t.deepEqual(k, k3, 'arbKey deepEqual 2nd custom unserialize');
+    t.true(keyEQ(k2, k3), '1st, 2nd custom unmarshal are keyEQ');
   }
 );
